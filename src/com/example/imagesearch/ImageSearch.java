8db37d0a8217844186fda3bcd899d86527ce01b8
Steve Paik <spaik@google.com>
2014-06-18 01:00:02 -0700

    Implemented Settings activity

--- src/com/example/imagesearch/ImageSearch.java
+++ src/com/example/imagesearch/ImageSearch.java
@@ -29,9 +29,10 @@ public class ImageSearch extends Activity {
 	GridView gvResults;
 	Button btnSearch;
 	String query;
+	String options = "";
 	ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
 	ImageResultArrayAdapter imageAdapter;
-	
+	static final int SETTINGS_REQUEST = 1234;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,90 +43,140 @@ public class ImageSearch extends Activity {
 		gvResults.setAdapter(imageAdapter);
 		gvResults.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Intent i = new Intent(getApplicationContext(), ImageDisplay.class);
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Intent i = new Intent(getApplicationContext(),
+						ImageDisplay.class);
 				ImageResult imageResult = imageResults.get(position);
-				i.putExtra("result",  imageResult);
+				i.putExtra("result", imageResult);
 				startActivity(i);
 			}
-			
+
 		});
-		
-        gvResults.setOnScrollListener(new EndlessScrollListener() {
-	        @Override
-	        public void onLoadMore(int page, int totalItemsCount) {
-	    		//Log.d("DEBUG", "Loading more items: " + totalItemsCount);
-	    		AsyncHttpClient client = new AsyncHttpClient();
-	    		client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
-	    					"start=" + totalItemsCount + "&v=1.0&q=" + Uri.encode(query),
-	    					new JsonHttpResponseHandler() {
-	    			@Override
-	    			public void onSuccess(JSONObject response) {
-	    				JSONArray imageJsonResults = null;
-	    				try {
-	    					imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
-	    					imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
-	    					Log.d("DEBUG", imageResults.toString());
-	    				} catch (JSONException e) {
-	    					e.printStackTrace();
-	    				}
-	    			}
-	    		});
-	        }
-        });
+
+		gvResults.setOnScrollListener(new EndlessScrollListener() {
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				// Log.d("DEBUG", "Loading more items: " + totalItemsCount);
+				AsyncHttpClient client = new AsyncHttpClient();
+				client.get(
+						"https://ajax.googleapis.com/ajax/services/search/images?rsz=8&"
+								+ "start=" + totalItemsCount + "&v=1.0&q="
+								+ Uri.encode(query) + options,
+						new JsonHttpResponseHandler() {
+							@Override
+							public void onSuccess(JSONObject response) {
+								JSONArray imageJsonResults = null;
+								try {
+									imageJsonResults = response.getJSONObject(
+											"responseData").getJSONArray(
+											"results");
+									imageAdapter.addAll(ImageResult
+											.fromJSONArray(imageJsonResults));
+									Log.d("DEBUG", imageResults.toString());
+								} catch (JSONException e) {
+									e.printStackTrace();
+								}
+							}
+						});
+			}
+		});
 
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present
 		getMenuInflater().inflate(R.menu.image_display, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	        case R.id.action_settings:
-	        	showSettingsPage();
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			showSettingsPage();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
-	
 	public void setupViews() {
 		etQuery = (EditText) findViewById(R.id.etQuery);
 		gvResults = (GridView) findViewById(R.id.gvResults);
 		btnSearch = (Button) findViewById(R.id.btnSearch);
-		
+
 	}
-	
+
 	public void onImageSearch(View v) {
 		query = etQuery.getText().toString();
-		Toast.makeText(this,  "Searching for " + query, Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG)
+				.show();
 		AsyncHttpClient client = new AsyncHttpClient();
-		client.get("https://ajax.googleapis.com/ajax/services/search/images?rsz=8&" + 
-					"start=" + 0 + "&v=1.0&q=" + Uri.encode(query),
-					new JsonHttpResponseHandler() {
-			@Override
-			public void onSuccess(JSONObject response) {
-				JSONArray imageJsonResults = null;
-				try {
-					imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
-					imageResults.clear();
-					imageAdapter.addAll(ImageResult.fromJSONArray(imageJsonResults));
-					//Log.d("DEBUG", imageResults.toString());
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		client.get(
+				"https://ajax.googleapis.com/ajax/services/search/images?rsz=8&"
+						+ "start=" + 0 + "&v=1.0&q=" + Uri.encode(query)
+						+ options, new JsonHttpResponseHandler() {
+					@Override
+					public void onSuccess(JSONObject response) {
+						JSONArray imageJsonResults = null;
+						try {
+							imageJsonResults = response.getJSONObject(
+									"responseData").getJSONArray("results");
+							imageResults.clear();
+							imageAdapter.addAll(ImageResult
+									.fromJSONArray(imageJsonResults));
+							// Log.d("DEBUG", imageResults.toString());
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+				});
 	}
-	
+
 	public void showSettingsPage() {
-		query = "start";
+		Intent i = new Intent(getApplicationContext(), ImageSettings.class);
+		startActivityForResult(i, SETTINGS_REQUEST);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode == RESULT_OK && requestCode == SETTINGS_REQUEST) {
+			// Generate the new options string
+			String itemText;
+			options = ""; 
+			
+			itemText = data.getExtras().getString("fileType");
+			if(!itemText.equals("any")) {
+				options += "&as_filetype=" + itemText;
+			}
+			
+			itemText = data.getExtras().getString("color");
+			if(!itemText.equals("any")) {
+				options += "&imgcolor=" + itemText;
+			}
+			
+			itemText = data.getExtras().getString("size");
+			if(!itemText.equals("any")) {
+				options += "&imgsz=" + itemText;
+			}
+			
+			itemText = data.getExtras().getString("type");
+			if(!itemText.equals("any")) {
+				options += "&imgtype=" + itemText;
+			}
+			
+			itemText = data.getExtras().getString("safeSearch");
+			if(!itemText.equals("off")) {
+				options += "&safe=" + itemText;
+			}
+			
+			itemText = data.getExtras().getString("site");
+			if(!itemText.equals("")) {
+				options += "&as_sitesearch=" + itemText;
+			}
+		}
 	}
 }
--- src/com/example/imagesearch/ImageSettings.java
+++ src/com/example/imagesearch/ImageSettings.java
@@ -1,9 +1,13 @@
 package com.example.imagesearch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ImageSettings extends Activity {
 
@@ -11,5 +15,52 @@ public class ImageSettings extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_image_settings);
+		
+		SetSpinnerAdapter(R.id.spFileType, R.array.file_type_array);
+		SetSpinnerAdapter(R.id.spColor, R.array.color_array);
+		SetSpinnerAdapter(R.id.spSize, R.array.size_array);
+		SetSpinnerAdapter(R.id.spType, R.array.type_array);
+		SetSpinnerAdapter(R.id.spSafeSearch, R.array.safe_search_array);
+		
+	}
+	
+	void SetSpinnerAdapter(int spinId, int stringArrayId) {
+		Spinner spinner = (Spinner) findViewById(spinId);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        stringArrayId, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+	}
+	
+	public void onSaveSetings(View v) {
+		Toast.makeText(this,  "Saved Settings", Toast.LENGTH_SHORT).show();
+		Spinner spinner;
+		
+		EditText etSite = (EditText)findViewById(R.id.etSite);
+		
+		// Create intent to send back to main activity
+		Intent data = new Intent();
+		
+		spinner = (Spinner) findViewById(R.id.spFileType);
+		data.putExtra("fileType",  spinner.getSelectedItem().toString());
+		
+		spinner = (Spinner) findViewById(R.id.spColor);
+		data.putExtra("color",  spinner.getSelectedItem().toString());
+
+		spinner = (Spinner) findViewById(R.id.spSize);
+		data.putExtra("size",  spinner.getSelectedItem().toString());
+
+		spinner = (Spinner) findViewById(R.id.spType);
+		data.putExtra("type",  spinner.getSelectedItem().toString());
+
+		spinner = (Spinner) findViewById(R.id.spSafeSearch);
+		data.putExtra("safeSearch",  spinner.getSelectedItem().toString());
+		
+		data.putExtra("site", etSite.getText().toString());
+		setResult(RESULT_OK, data);
+		finish();
 	}
 }
